946445a5a08b50bb0aac6fde1149cb0c91fceee1
jfosterjr99 <125596374+jfosterjr99@users.noreply.github.com>
2023-05-12 10:58:43 -0400

    Delete psql_create_msi.sql

--- Data/psql_create_msi.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS msi_alignment_scores;
-CREATE TABLE msi_alignment_scores
-(
-  gene_name  VARCHAR(20) NOT NULL,
-  msi_val REAL NOT NULL,
-  upperalignment     VARCHAR(100) NOT NULL,
-  loweralignment       VARCHAR(100) NOT NULL,
-  sample       VARCHAR(100) NOT NULL
-);
